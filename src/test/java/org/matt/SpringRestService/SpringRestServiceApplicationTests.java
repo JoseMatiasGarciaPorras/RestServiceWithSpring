0b3d4bd7877884847f4a1d16508188c410767e79
José Matías García <jmgarciaporras@gmail.com>
2020-05-25 16:59:53 +0200

    Creación del proyeco

---
+++ src/test/java/org/matt/SpringRestService/SpringRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.matt.SpringRestService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
